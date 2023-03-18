5218eba2cb2203420e785a70667bd2ffe6940020
SwapnilV26 <swapnilvish2812@gmail.com>
2023-03-19 03:12:34 +0530

    Landing Page and Codeframe

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,13 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Code from './components/Code.js';
 import App from './App';
 import MainShop from './components/MainShop';
 import ProductDetails from './components/productDetails';
 import LandingPage from './components/LandingPage';
 
-ReactDOM.render(
-  <React.StrictMode>
-<<<<<<< HEAD
-    <LandingPage />
-  </React.StrictMode>
-=======
-    <Code />
-  </React.StrictMode>,
-  document.getElementById('root')
->>>>>>> 07b888bd9a46f6ae00dca1c5586026aeb421c2f7
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <App/>
 );
 
