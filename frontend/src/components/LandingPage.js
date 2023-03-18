aab106037b16fcac69b2cc72438f2d47d783d5e0
SwapnilV26 <swapnilvish2812@gmail.com>
2023-03-18 16:15:14 +0530

    login page added

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,8 +1,20 @@
 import React from "react";
+import Navbar from "./Navbar";
+import Profile from "./Profile";
 
 const LandingPage = () => {
   return (
     <>
+      <Navbar />
+      <div className="flex flex-row">
+        <div>
+          <Profile />
+        </div>
+        <div>
+          Main content
+        </div>
+      </div>
+
       <div className="landing-main-container">
         <div className="quote-section">
           <h1>"When the customer comes first, the customer will last."</h1>
--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "../css/Index.css";
+import user from '../utils/img/user.png'
+
 const Profile = ()=>{
     return(
         <>
         <div className="profile-container">
-            <div className="profile-picture"></div>
-            <div className="profile-description"></div>
+            <div className="profile-picture">
+                <img className="w-10 profile-picture" src={user} alt="user" />
+            </div>
+            <div className="profile-description">
+                <h1 className="text-blue-500">Helllo</h1>
+            </div>
           </div>
         </>
     )
